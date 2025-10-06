4319e861d66994c317a9ba86eac4608c9f044467
Pierre LaMogne <pierrelamogne@gmail.com>
2025-10-06 15:24:10 +0200

    Add localization of ErrorMessages in the Order page

--- P2FixAnAppDotNetCode/Resources/Models/ViewModels/Resources.cs
+++ P2FixAnAppDotNetCode/Resources/Models/ViewModels/Resources.cs
@@ -3,9 +3,9 @@ using System.Resources;
 using System.Reflection;
 using System.Globalization;
 
-namespace P2FixAnAppDotNetCode.Resources.Models.ViewModels
+namespace P2FixAnAppDotNetCode.Resources.Models.ViewModels.Resources
 {
-    public static class Order
+    public static class ErrorMessages
     {
         private static ResourceManager resourceManager = new ResourceManager("P2FixAnAppDotNetCode.Resources.Models.ViewModels.Order", Assembly.GetExecutingAssembly());
         private static CultureInfo resourceCulture;
