8a57429474847c8a994f1f9458bc09d0bda1f4b3
Pierre LaMogne <pierrelamogne@gmail.com>
2025-10-06 15:08:44 +0200

    Add FindProductInCartLines() method

--- P2FixAnAppDotNetCode/Models/Cart.cs
+++ P2FixAnAppDotNetCode/Models/Cart.cs
@@ -76,8 +76,7 @@ namespace P2FixAnAppDotNetCode.Models
         /// </summary>
         public Product FindProductInCartLines(int productId)
         {
-            // TODO implement the method
-            return null;
+            return GetCartLineList().FirstOrDefault(item => item.Product.Id == productId)?.Product;
         }
 
         /// <summary>
